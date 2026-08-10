b341535ffa67b84592ba3fdbb8b87ab8549ac79f
Kyathi <kyathiganjala@gmail.com>
2026-08-10 23:32:17 +0530

    Datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("helloo"); // we are using node.js not browser
+
+console.log(3+3);
+
+let name = "kyathi";
+
+let age = 20
+
+let login = false
+
+console.log(typeof login);
+//null => standalone value -> special type (Khaali);
+//undefined => means value not defined.
+//symbol => unique elements keliye
+
+
