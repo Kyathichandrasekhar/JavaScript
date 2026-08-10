38758fe51308f0c064fe1613ad5c243a08ce0500
Kyathi <kyathiganjala@gmail.com>
2026-08-11 00:23:08 +0530

    Datatype Conversions

---
+++ 01basics/03datatypesConversion.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+console.log(typeof score);
+
+let ConversiontoNumber = Number(score);//number convertion
+
+console.log(typeof ConversiontoNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "chinnu";
+
+let booleanLogin = Boolean(isLoggedIn);
+
+console.log(booleanLogin);
+
+// 1 => true; 0 => false;
+//"" => false
+// "kyathi" => true;
