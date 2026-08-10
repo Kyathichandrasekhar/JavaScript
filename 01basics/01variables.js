1643569b915198e0f6ba6e3f50e491a464ed3e64
Kyathi <kyathiganjala@gmail.com>
2026-08-10 23:07:36 +0530

    Learned Variable and Constants

---
+++ 01basics/01variables.js
@@ -0,0 +1,23 @@
+const account_id = 14453;
+let accoun_Email = "kyathiganjala@gmail.com"
+var account = "12345"
+accountCity = "jaipur" //possible 
+let accountState;
+
+// account_id = 2
+accoun_Email = "kyathi@gmail.com"
+account = "15"
+console.log(account_id);
+console.log(accoun_Email);
+console.table([accoun_Email,account,account_id,accountState]);
+
+
+// {
+//     this is a scope
+// }
+
+/* 
+    Prefer not to use var 
+    because of issue in  block scope and functional scope 
+*/
+
