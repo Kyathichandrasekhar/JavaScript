ac530452004ad24ae4e0a9b8de5d4d3c6e704280
Kyathi <kyathiganjala@gmail.com>
2026-08-21 22:35:37 +0530

    Functions

---
+++ functions/01functions.js
@@ -0,0 +1,42 @@
+// // //functions are nothing but a block of code that can be reused in a program. Functions are used to perform a specific task. Functions are defined using the function keyword followed by the name of the function and parentheses. The code block is enclosed in curly braces {}.
+
+// // function saymyname(){
+// //     console.log("K");
+// //     console.log("y");
+// //     console.log("a");
+// //     console.log("t");
+// //     console.log("h");
+// //     console.log("i");   
+// // }
+
+// // function addTwoNumbers(number1 , number2){//no need to give data type
+// //     console.log(number1 + number2);
+// // }
+
+// // addTwoNumbers(3,5);
+
+// // function addTwoNumbers(number1 , number2){//no need to give data type
+// //    let result = number1 + number2;
+// //    return result;
+// // }
+
+// // const result = addTwoNumbers(3,9);
+
+// // console.log("Result:",result);
+
+// function loginuser(username) {
+//     return `${username} just logged in!`
+// }
+
+// console.log(loginuser("Kyathi"));
+
+
+//accessing array 
+
+const mynewarray = [100,200,300,400,500];
+
+function array(nums){
+    return nums[2];
+}
+
+console.log(array(mynewarray));
