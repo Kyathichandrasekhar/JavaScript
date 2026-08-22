f461602c76c76b0ec114a2dbbe0f5db3f94d9ccf
Kyathi <kyathiganjala@gmail.com>
2026-08-22 22:57:05 +0530

    folder changes !

--- functions/01functions.js
+++ functions/01functions.js
@@ -1,34 +1,34 @@
-// // //functions are nothing but a block of code that can be reused in a program. Functions are used to perform a specific task. Functions are defined using the function keyword followed by the name of the function and parentheses. The code block is enclosed in curly braces {}.
+// //functions are nothing but a block of code that can be reused in a program. Functions are used to perform a specific task. Functions are defined using the function keyword followed by the name of the function and parentheses. The code block is enclosed in curly braces {}.
 
-// // function saymyname(){
-// //     console.log("K");
-// //     console.log("y");
-// //     console.log("a");
-// //     console.log("t");
-// //     console.log("h");
-// //     console.log("i");   
-// // }
-
-// // function addTwoNumbers(number1 , number2){//no need to give data type
-// //     console.log(number1 + number2);
-// // }
-
-// // addTwoNumbers(3,5);
-
-// // function addTwoNumbers(number1 , number2){//no need to give data type
-// //    let result = number1 + number2;
-// //    return result;
-// // }
-
-// // const result = addTwoNumbers(3,9);
-
-// // console.log("Result:",result);
-
-// function loginuser(username) {
-//     return `${username} just logged in!`
+// function saymyname(){
+//     console.log("K");
+//     console.log("y");
+//     console.log("a");
+//     console.log("t");
+//     console.log("h");
+//     console.log("i");   
 // }
 
-// console.log(loginuser("Kyathi"));
+// function addTwoNumbers(number1 , number2){//no need to give data type
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3,5);
+
+// function addTwoNumbers(number1 , number2){//no need to give data type
+//    let result = number1 + number2;
+//    return result;
+// }
+
+// const result = addTwoNumbers(3,9);
+
+// console.log("Result:",result);
+
+function loginuser(username) {
+    return `${username} just logged in!`
+}
+
+console.log(loginuser("Kyathi"));
 
 
 //accessing array 
@@ -40,3 +40,14 @@ function array(nums){
 }
 
 console.log(array(mynewarray));
+
+
+//function decleration type 2
+
+const Anyvariable = function(name) {
+    return name + " is very good boy";
+}
+
+console.log(Anyvariable("Kyathi"));
+
+
