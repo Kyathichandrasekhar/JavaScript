233d517352649de5d433754ee5ab724d27dbca4f
Kyathi <kyathiganjala@gmail.com>
2026-08-23 19:30:38 +0530

    Arrow funtions and IIFE

---
+++ Arrow functions/arrowfunction.js
@@ -0,0 +1,69 @@
+//Arrow functions are a more concise way to 
+// write functions in JavaScript. 
+// They are often used for short, 
+// single-line functions, 
+// and they do not have their own 'this' context.
+
+const user = {
+    username : "kyathi",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} , welcome to the website`);
+    }
+}
+
+user.welcomeMessage()
+user.username = "chinnu"
+user.welcomeMessage()
+
+function chai(){
+    let username  = "kyathi"
+    console.log(this.username);// it wont work under functions, only work in object.
+}
+
+chai();
+
+const chai = function () {
+    //   normal function
+}
+
+//arrow function
+
+const chai = () => {
+    let username = "kyathi";
+    console.log(this.username);
+}
+
+chai()
+
+//shorter EXPRESSION 
+
+const add = (a, b) => a + b;
+
+console.log(add(10, 20));
+
+// // Normal function:
+
+// // "this depends on how I am called."
+
+// // Arrow function:
+
+// // "this depends on where I was created.
+
+// => **** You only need a normal function if you specifically want the arrow function to inherit a useful this from that normal function. ***
+
+// const person = {
+//     name: "Khyathi",
+
+//     greet: function() {
+
+//         const arrow = () => {
+//             console.log(this.name);
+//         };
+
+//         arrow();
+//     }
+// };
+
+// person.greet();
---
+++ IIFE(immediately Invoked Function expressions/iife.js
@@ -0,0 +1,24 @@
+// Normal function
+
+function chai(){
+    console.log("DB connected");
+}
+
+chai()
+
+// To invoke Immediately and ***Avoid polluting the global scope***
+
+(function(){
+    console.log(`DB connected`);
+})();
+
+// (function)(execution)
+
+// writing like arrow function 
+
+( () => {
+    console.log('DB connected 2');
+})();
+
+
+// proper def : IIFE (Immediately Invoked Function Expression) is a JavaScript function expression that is defined and executed immediately after it is created.
