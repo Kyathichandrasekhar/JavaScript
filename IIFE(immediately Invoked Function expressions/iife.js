9f8b4b98d25eab7874a8f699ecc6d8eeebd6d83e
Kyathi <kyathiganjala@gmail.com>
2026-08-23 21:15:10 +0530

    Changes in IIFE

--- IIFE(immediately Invoked Function expressions/iife.js
+++ IIFE(immediately Invoked Function expressions/iife.js
@@ -20,5 +20,10 @@ chai()
     console.log('DB connected 2');
 })();
 
+//*********************** */
+
+(function(myname){
+    console.log(`${myname} is a good boy`);
+})("Khyathi");
 
 // proper def : IIFE (Immediately Invoked Function Expression) is a JavaScript function expression that is defined and executed immediately after it is created.
